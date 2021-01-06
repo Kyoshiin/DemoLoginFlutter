ae149579e13b7703272d18f6c4054531ab1c10cd
Pratyay <pratyayroy18@gmail.com>
2021-01-06 19:01:29 +0530

    updates

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:second_app/RegScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatelessWidget {
   // String name = '';
 
   //fr FORM
-  var _formkey = GlobalKey<FormState>(); // key to uniquely identify a widget; also handles the state
+  var _formkey = GlobalKey<
+      FormState>(); // key to uniquely identify a widget; also handles the state
   TextEditingController _phnController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
+  static const mPhn = "7003052";
+  static const mPs = "";
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +25,16 @@ class LoginScreen extends StatelessWidget {
             child: Column(
               children: [
                 Center(
-                  child: Image.network(
-                    "https://www.charislms.com/images/slogin.png",
-                    width: 200,
-                    height: 250,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 80),
+                    child: CircleAvatar(
+                      child: Image.network(
+                        "https://www.charislms.com/images/slogin.png",
+                        // width: 200,
+                        // height: 250,
+                      ),
+                      radius: 60,
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -40,31 +51,30 @@ class LoginScreen extends StatelessWidget {
                     //   print(v);
                     // },
                     decoration: InputDecoration(
-                        hintText: '+91',
-                        labelText: 'Enter your Contact No.',
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            width: 5,
-                            color: Colors.green[900],
-                          ),
-                          borderRadius: BorderRadius.circular(12),
+                      hintText: '+91',
+                      labelText: 'Enter your Contact No.',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 5,
+                          color: Colors.green[900],
                         ),
-                        // errorText: name.isEmpty?'Please input a name':null,
-                        icon: Icon(Icons.person),),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      // errorText: name.isEmpty?'Please input a name':null,
+                      icon: Icon(Icons.person),
+                    ),
 
                     keyboardType: TextInputType.phone,
-                    validator: (val){
-                      if(val.isEmpty)
+                    validator: (val) {
+                      if (val.isEmpty)
                         return 'Please enter your no';
-
-                      else if(val.length<10)
-                        return 'Number must be of 5 characters atleast';
+                      else if (val.length < 5)
+                        return 'Number must be of 10 characters atleast';
 
                       return null;
                     },
                   ),
                 ),
-
                 SizedBox(
                   height: 10,
                 ),
@@ -89,47 +99,70 @@ class LoginScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(12),
                       ),
                       // errorText: name.isEmpty?'Please input a name':null,
-                      icon: Icon(Icons.enhanced_encryption),),
+                      icon: Icon(Icons.enhanced_encryption),
+                    ),
 
                     keyboardType: TextInputType.text,
-                    validator: (val){
-                      if(val.isEmpty)
+                    validator: (val) {
+                      if (val.isEmpty)
                         return 'Please enter your password';
-
-                      else if(val.length<5)
+                      else if (val.length < 5)
                         return 'Password must be of 5 characters atleast';
 
-                        return null;
+                      return null;
                     },
                     obscureText: true,
                   ),
                 ),
-
-                FlatButton(onPressed: ()=>checkSignIn(),
+                FlatButton(
+                  onPressed: () => checkSignIn(context),
                   height: 40,
                   minWidth: 200,
                   color: Colors.cyan[800],
-                  child: Text('Sign In',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16
-                  ),),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  side: BorderSide(
-                    width: 3,
-                    color: Colors.cyan
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
-                ),),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    side: BorderSide(width: 3, color: Colors.cyan),
+                  ),
+                ),
               ],
             ),
           ),
         ));
   }
 
-  checkSignIn() {
-    if(_formkey.currentState.validate()) // validator
-        print('Phone: ${_phnController.text}\n'
-            'Password:  ${_passwordController.text}');
+  checkSignIn(BuildContext con) async {
+    if (_formkey.currentState.validate()) {
+      // validator
+      String phn = _phnController.text;
+      String ps = _passwordController.text;
+
+      if (phn.trim().isNotEmpty) {
+        //success
+        print("inSuccess");
+        Scaffold.of(con).showSnackBar(
+          SnackBar(
+            content: Text('Login successful'),
+          ),
+        );
+
+        SharedPreferences sp = await SharedPreferences.getInstance();
+        await sp.setString('phone', phn.trim());
+
+        //navigate
+        Navigator.of(con).pushReplacement(MaterialPageRoute(builder: (con) => RegScreen()));
+
+      } else {
+        showDialog(
+          context: con,
+          builder: (con) => AlertDialog(
+            content: Text('Login failed!'),
+          ),
+        );
+      }
+    }
   }
 }
